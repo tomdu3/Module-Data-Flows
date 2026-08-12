45874f4fe008ab1b0930dc3dce6473aa5ef75ea8
Tomislav Dukez <tomdu3@ymail.com>
2026-08-12 12:04:27 +0100

    add error handling implementation and default image

--- fetch/programmer-humour/script.js
+++ fetch/programmer-humour/script.js
@@ -1,13 +1,26 @@
 const imgEl = document.getElementsByTagName("img")[0];
-const endpointAPI = "https://xkcd.now.sh/?comic=latest";
+const endpointAPI = "https://xkcd.now.ssh/?comic=latest";
 
 const fetchData = async () => {
-  const response = await fetch(endpointAPI);
-  return await response.json();
+  try {
+    const response = await fetch(endpointAPI);
+    if (!response.ok) {
+      throw new Error(`Network response status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Error fetching data:", error);
+    return null;
+  }
 };
 
 let data;
-fetchData().then((object) => {
-  console.log(object);
-  imgEl.src = object.img || "";
+fetchData().then((data) => {
+  console.log(data);
+  if (data && data.img) {
+    imgEl.src = data.img;
+  } else {
+    console.error("Image data not received");
+  }
 });
