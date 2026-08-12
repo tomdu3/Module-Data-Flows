5db507ce0871ef989e0d3ecfb8cf7c7b874f6ec5
Tomislav Dukez <tomdu3@ymail.com>
2026-08-12 11:43:33 +0100

    add fetch image and render to html

--- fetch/programmer-humour/script.js
+++ fetch/programmer-humour/script.js
@@ -1,2 +1,13 @@
-document.getElementsByTagName("h1")[0].innerText =
-  "Hello from the other side!!!";
+const imgEl = document.getElementsByTagName("img")[0];
+const endpointAPI = "https://xkcd.now.sh/?comic=latest";
+
+const fetchData = async () => {
+  const response = await fetch(endpointAPI);
+  return await response.json();
+};
+
+let data;
+fetchData().then((object) => {
+  console.log(object);
+  imgEl.src = object.img || "";
+});
